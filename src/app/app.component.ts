bac0291f31474504811f59e197056c20d1f0b2f1
liceist2008@mail.ru <liceist2008@mail.ru>
2019-11-08 16:30:03 +0600

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web2';
+export class AppComponent implements OnInit {
+  private _hubConnection: HubConnection;
+  nick = '';
+  message = '';
+  messages: string[] = [];
+
+  public sendMessage(): void {
+    this._hubConnection
+      .invoke('sendToAll', this.nick, this.message)
+      .then(() => this.message = '')
+      .catch(err => console.error(err));
+  }
+  ngOnInit() {
+
+    this.nick = window.prompt('Your name:', 'John');
+
+    let hubUrl = 'http://localhost:5000/chat';
+    this._hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(hubUrl)
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this._hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+      const text = `${nick}: ${receivedMessage}`;
+      this.messages.push(text);
+    });
+  }
+  
 }
+
+
